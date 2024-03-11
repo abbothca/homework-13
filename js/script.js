311711daa45e9e6f11fe20868c4e6b231b722d86
Liubov Khomych <kroka.luba@gmail.com>
2024-03-11 16:52:05 +0200

    after review #1

--- js/script.js
+++ js/script.js
@@ -8,35 +8,40 @@ const elementDOMTimeInfo = {
 };
 let spanToState = switcherButton.querySelector(".switcher__to-state");
 
- //якщо в localStorage вже є запис про активний стан: задаємо початковий стан сторінки - покажемо потрібний з блоків
-localStorage.getItem("state-active")
-    ? updateSwitcher(notState(localStorage.getItem("state-active"))) : false;    
+//якщо в localStorage вже є запис про активний стан: задаємо початковий стан сторінки - покажемо потрібний з блоків
+let activeState = localStorage.getItem("state-active");
+if (activeState) {
+    updateSwitcher(toggleState(activeState));
+    //окремо треба прорахувати чи треба клас при завантаженні сторінки
+    (activeState === "on")
+        ? main.classList.add("is-dark") : main.classList.remove("is-dark");
+};
+
 
 //обробник на подію click для кнопки
-switcherButton?.addEventListener("click", (event) => {
+switcherButton.addEventListener("click", (event) => {
     const previousButtonState = localStorage.getItem("state-active") || "off"; //попередній стан ( || для першого виклику, коли в localStorage нема значення)   
-    localStorage.setItem(notState(previousButtonState), Date.now()); //записати в localStorage час останньої події
-    localStorage.setItem("state-active", notState(previousButtonState)); //записати в localStorage тип останньої події
+    localStorage.setItem(toggleState(previousButtonState), Date.now()); //записати в localStorage час останньої події
+    localStorage.setItem("state-active", toggleState(previousButtonState)); //записати в localStorage тип останньої події
     updateSwitcher(previousButtonState); //оновити сторінку після перемикання
 })
 
 // змінюємо on -> off і навпаки off -> on
-function notState(state) {
+function toggleState(state) {
     return (state === "on") ? "off" : "on";
 }
 
 //оновлюємо стан сторінки
 function updateSwitcher(state) {
     //змінюємо видімість блоків з часом останнього вмикання/вимикання
-    elementDOMTimeInfo[state].hidden = false; 
-    elementDOMTimeInfo[notState(state)].hidden = true;
+    elementDOMTimeInfo[state].hidden = false;
+    elementDOMTimeInfo[toggleState(state)].hidden = true;
     //клас до body
-    (state === "off")
-        ? main.classList.add("is-dark") : main.classList.remove("is-dark");
+    main.classList.toggle("is-dark");
     spanToState.textContent = state;    //змінюємо напис на кнопці
     //оновлюємо час попередної дії для видимого блоку
     elementDOMTimeInfo[state].querySelector('.switcher__time').textContent
-        = getFormattedTime(+localStorage.getItem(notState(state))); 
+        = getFormattedTime(+localStorage.getItem(toggleState(state)));
 }
 
 //додаємо попереду "0", якщо маємо одноцифрове число, щоб отримати двоцифрове
